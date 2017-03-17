83663a91f3f8154696b1e611678f8c48f485bdbe
Jack Allcock <allcockj@cardiff.ac.uk>
2017-03-17 22:01:19 +0000

    Layout slightly changed, buttons at the bottom for better reach. ClickListeners added for the search button and a list view added. When someone enters a postcode it is validated and loads a list of museums.

--- app/src/main/java/nsa/com/museum/HistoryActivity.java
+++ app/src/main/java/nsa/com/museum/HistoryActivity.java
@@ -3,7 +3,7 @@ package nsa.com.museum;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class History extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/nsa/com/museum/MainActivity.java
+++ app/src/main/java/nsa/com/museum/MainActivity.java
@@ -4,33 +4,84 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> museums = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    EditText postcodeInput;
+    ListView museumsList;
+    Button searchBtn;
+    Button settingsBtn;
+    Button historyBtn;
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        postcodeInput = (EditText) findViewById(R.id.editSearch);
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        museumsList = (ListView) findViewById(R.id.museumsList);
+        settingsBtn = (Button) findViewById(R.id.settingsBtn);
+        historyBtn = (Button) findViewById(R.id.historyBtn);
+        nextBtn = (Button) findViewById(R.id.nextBtn);
 
-        Button settingsbtn = (Button) findViewById(R.id.settingsbtn);
-        settingsbtn.setOnClickListener(new View.OnClickListener() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String postcode = postcodeInput.getText().toString();
+                if (postcode.length() < 6 ) {
+                    Toast error = Toast.makeText(getApplicationContext(), "Please enter a 6 character postcode", Toast.LENGTH_SHORT);
+                    error.show();
+                }
+                else {
+                    Toast nearYou = Toast.makeText(getApplicationContext(), "Loaded Museums near you", Toast.LENGTH_SHORT);
+                    nearYou.show();
+                    museums.add("Cardiff");
+                    museums.add("Newport");
+                    museums.add("Birmingham");
+                    museums.add("London");
+                    adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, museums);
+                    museumsList.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    ((EditText)findViewById(R.id.editSearch)).setText(" ");
+                }
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
                 startActivity(i);
             }
         });
+
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HistoryActivity.class);
+                startActivity(i);
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), BeaconActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
-//    public void goSettings(View view){
-//        Button settingsbtn = (Button) findViewById(R.id.settingsbtn);
-//        settingsbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
-//                startActivity(i);
-//            }
-//        });
-
 }
+

e4f9bef38633a02d47dc7675519c7a019a62f9e0
Jack Allcock <allcockj@cardiff.ac.uk>
2017-03-18 18:13:20 +0000

    Long item click adds beacon to history

--- app/src/main/java/nsa/com/museum/BeaconActivity.java
+++ app/src/main/java/nsa/com/museum/BeaconActivity.java
@@ -2,10 +2,13 @@ package nsa.com.museum;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,13 +23,16 @@ import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBeaconRegion;
 import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBeaconScanManager;
 import com.gcell.ibeacon.gcellbeaconscanlibrary.GCelliBeacon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BeaconActivity extends AppCompatActivity implements GCellBeaconManagerScanEvents {
 
     GCellBeaconScanManager scanMan;
     ListView lv;
-    ArrayAdapter adap;
+    ArrayAdapter beaconAdap;
+    ArrayList <String> beacontest = new ArrayList<>();
+    ArrayList <String> historyBeacons = new ArrayList<>();
 
     int PERM_CODE = 101;
     String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN};
@@ -40,8 +46,16 @@ public class BeaconActivity extends AppCompatActivity implements GCellBeaconMana
         checkPermissions();
 
         //Create adapter for beacons
-        adap = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1);
-        lv.setAdapter(adap);
+        beaconAdap = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, beacontest);
+
+        lv.setAdapter(beaconAdap);
+
+        // Simulating beacons in the LV for when working outside away from NSA
+        beacontest.add("6466346446223");
+        beacontest.add("90090990800");
+        beacontest.add("473474242446");
+        beaconAdap.notifyDataSetChanged();
+
         //Create a manager with the application context
         scanMan = new GCellBeaconScanManager(this);
         scanMan.enableBlueToothAutoSwitchOn(true);
@@ -52,17 +66,47 @@ public class BeaconActivity extends AppCompatActivity implements GCellBeaconMana
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), adap.getItem(i).toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), beaconAdap.getItem(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View view, int i, long l) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(BeaconActivity.this);
+                builder.setTitle("Options");
+                final String item = beaconAdap.getItem(i).toString();
+                builder.setItems(new String[]{"Add to history", "Cancel"}, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                Toast.makeText(getApplicationContext(),item + " Added to history", Toast.LENGTH_SHORT).show();
+                                historyBeacons.add(item);
+                                Intent intent = new Intent(getBaseContext(), HistoryActivity.class);
+                                intent.putStringArrayListExtra("beacons", historyBeacons);
+                                beaconAdap.notifyDataSetChanged();
+                                startActivity(intent);
+                                break;
+                            case 1:
+
+                                break;
+
+                        }
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
     }
 
+
     @Override
     public void onGCellUpdateBeaconList(List<GCelliBeacon> list) {
         for (GCelliBeacon beacon : list) {
-            if(adap.getPosition(beacon.getProxUuid().getStringFormattedUuid()) == -1) {
+            if(beaconAdap.getPosition(beacon.getProxUuid().getStringFormattedUuid()) == -1) {
 
-                adap.add(beacon.getProxUuid().getStringFormattedUuid());
+                beaconAdap.add(beacon.getProxUuid().getStringFormattedUuid());
             }
         }
     }
--- app/src/main/java/nsa/com/museum/HistoryActivity.java
+++ app/src/main/java/nsa/com/museum/HistoryActivity.java
@@ -2,12 +2,24 @@ package nsa.com.museum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    ListView historyLv;
+    ArrayAdapter historyAdap;
+    ArrayList<String> historyBeacons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        historyBeacons = getIntent().getExtras().getStringArrayList("beacons");
+        historyAdap = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, historyBeacons);
+        historyLv = (ListView) findViewById(R.id.historyView);
+        historyLv.setAdapter(historyAdap);
     }
 }

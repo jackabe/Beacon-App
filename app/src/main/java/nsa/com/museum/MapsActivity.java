a63a46baff44b27d5097bebddee6a023b58ba4e0
Laura Vuilleumier <vuilleumierl@cardiff.ac.uk>
2017-03-20 21:37:02 +0000

    Fixed crash on map start. Map now shows current location (hardcoded - not yet complete).

--- app/src/main/java/nsa/com/museum/MapsActivity.java
+++ app/src/main/java/nsa/com/museum/MapsActivity.java
@@ -38,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng currentLocation = new LatLng(51.589082, -3.0036177);
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("You are here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(9));
     }
 }

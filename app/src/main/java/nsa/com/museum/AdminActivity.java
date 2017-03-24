85e1b6308c5d83acca84f26a479410734ad2c424
Jack Allcock <allcockj@cardiff.ac.uk>
2017-03-24 18:03:52 +0000

    Password now has to be password to gain access. Admin page now has forms to add museums and beacons

--- app/src/main/java/nsa/com/museum/AdminActivity.java
+++ app/src/main/java/nsa/com/museum/AdminActivity.java
@@ -1,18 +1,60 @@
 package nsa.com.museum;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class AdminActivity extends Activity {
+public class AdminActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent settings = new Intent(getApplicationContext(), NewSettingsActivity.class);
+                startActivity(settings);
+                return true;
+
+            case R.id.action_help:
+                Intent help = new Intent(getApplicationContext(), HelpActivity.class);
+                startActivity(help);
+                return true;
+
+            case R.id.action_login:
+                Intent login = new Intent(getApplicationContext(), AdminLogin.class);
+                startActivity(login);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
